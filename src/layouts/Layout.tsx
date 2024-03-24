7227fca23148b6543e9828008205d8c31857468b
egsir1 <egsir111@gmail.com>
2024-03-24 14:56:52 +0900

    feat: Layout & Header & Footer components publishing

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
 
+interface Props {
+    children: React.ReactNode
+}
 
-
-const Layout = () => {
+const Layout = ({children}:Props) => {
   return (
-    <div>Layout</div>
+    <div className='flex flex-col min-h-screen'>
+        <Header/>
+        <Hero/>
+        <div className="container mx-auto py-10 flex-1">
+            {children}
+        </div>
+        <Footer/>
+    </div>
   )
 }
 
