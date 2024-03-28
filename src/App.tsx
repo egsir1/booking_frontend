1641eeeac2fdf52ef07e31f01c0b02a5eb92c670
egsir1 <egsir111@gmail.com>
2024-03-28 09:25:03 +0900

    feat: user registration completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Register from './pages/Register'
 
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
     </Layout>}/>
     <Route path='/search' element={<Layout>
       <p>Search Page</p>
+    </Layout>} />
+    <Route path='/register' element={<Layout>
+      <Register/>
     </Layout>} />
     <Route path='*' element={<Navigate to='/' />} />
   </Routes>
