bad6601b2f67c72058e9cd9631fa9c97813da571
egsir1 <egsir111@gmail.com>
2024-04-06 12:03:01 +0900

    feat: ManageHotelForm UI publishing

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import * as apiClient from "../api-client";
 import { useMutation, useQueryClient } from "react-query";
 import { useAppContext } from "../contexts/AppContext";
